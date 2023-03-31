de1392cff3a27f49cc8d61e86835bf9e0e766fad
Warren Moore <34307422+WarrenMoore33@users.noreply.github.com>
2023-03-31 17:43:41 -0400

    Add files via upload
    all app components added
    make sure to run npm install to get your node modules and then npm run serve to see your vue work

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+    <the-header :title="'RememberMe'"></the-header>
+    <the-resources></the-resources>    
+</template>
+
+<script>
+import  TheHeader  from './components/layouts/TheHeader.vue'
+import  TheResources  from './components/learning-resources/TheResources.vue'
+
+export default ({
+    components: {
+        TheHeader,
+        TheResources
+    },
+    methods: {
+
+    },
+    setup() {
+        
+    },
+})
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+/* Transition Element 
+        Styling
+    Transition Animations 
+*/
+
+/* Default tansition animation */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+/* Fade animation */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+/* Slide Fade animation */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+/* Bounce animation */
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+
+</style>
